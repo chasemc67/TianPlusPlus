3e6456887852eef6a589f2829b0bfa68a72e5b6b
chasemc67 <chasemc67@gmail.com>
2016-10-08 14:53:40 -0600

    Have some sample llvm code that will print stuff now

--- src/LLVMVisitor.java
+++ src/LLVMVisitor.java
@@ -1,6 +1,60 @@
 import java.util.HashMap;
 import java.util.Map;
+import org.stringtemplate.v4.*;
 
-public class LLVMVisitor extends VCalcBaseVisitor<Integer> {
+public class LLVMVisitor extends VCalcBaseVisitor<Void> {
+	STGroup group = new STGroupFile("./src/StringTemplates/llvm.stg");
+	
+	// The starting boilerplate for the assembly program
+	ST st = group.getInstanceOf("prog");
+	String beginProg = st.render();
+
+	// The global variable declarations of the program
+	String variableDeclarations = "";
+
+	// The start of main
+	ST st2 = group.getInstanceOf("main");
+	String initMain = st2.render();
+
+	// The main body of the program
+	String programBody = "";
+
+	// Code to safely exit program
+	ST st3 = group.getInstanceOf("exit");
+	String exitProgram = st3.render();
+
+
+	@Override 
+	public Void visitProg(VCalcParser.ProgContext ctx) {
+		// Have the children all write their subroutines to the program
+		// body and variable declarations
+		visitChildren(ctx);
+
+		//Compile final program and print
+		String output = beginProg + variableDeclarations + "\n" + initMain + programBody + exitProgram;
+		System.out.println(output);
+		return null;
+	}
+
+	@Override
+	public Void visitPrint(VCalcParser.PrintContext ctx) {
+	    // Get the value that we'll be printint out onto the stack
+	    visit(ctx.expr());
+
+	    // Will need to add some check here for whether we're
+	    // printing an int or vec
+	    ST output = group.getInstanceOf("testPrintIntFromStack");
+	    programBody = programBody + "\n" + output.render();
+	    return null;
+	}
+
+	@Override
+	public Void visitExprInt(VCalcParser.ExprIntContext ctx) {
+	    Integer intValue = Integer.valueOf(ctx.INTEGER().getText());
+	    ST output = group.getInstanceOf("testWriteIntToStack");
+	    //ST output2 = output.add("intValue", intValue);
+	    programBody = programBody + "\n" + output.render();
+	    return null;
+	}
 
 }
