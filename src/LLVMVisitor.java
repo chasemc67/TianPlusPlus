ceacc2497fdad85a39be53ea2af435a2e6a9c271
chasemc67 <chasemc67@gmail.com>
2016-10-08 18:55:33 -0600

    Integer asn almost working, see visitExprId, visitDeclAsn, visitAssignment

--- src/LLVMVisitor.java
+++ src/LLVMVisitor.java
@@ -3,6 +3,10 @@ import java.util.Map;
 import org.stringtemplate.v4.*;
 
 public class LLVMVisitor extends VCalcBaseVisitor<Void> {
+
+	Scope scope = new Scope();
+	Integer scopeCounter = 0;
+
 	STGroup group = new STGroupFile("./src/StringTemplates/llvm.stg");
 	
 	// The starting boilerplate for the assembly program
@@ -25,6 +29,8 @@ public class LLVMVisitor extends VCalcBaseVisitor<Void> {
 
 	// main uses 3 of these right off the bat
 	Integer varCounter = 3;
+	
+	Map<String, Integer> userVarCounter = new HashMap<String, Integer>();
 
 
 	@Override 
@@ -41,29 +47,45 @@ public class LLVMVisitor extends VCalcBaseVisitor<Void> {
 
 	@Override
     public Void visitDeclAsn(VCalcParser.DeclAsnContext ctx) {
-    	String userDefinedName = ctx.assignment.ID().getText();
+    	String userDefinedName = ctx.assignment().ID().getText();
+
+    	if (userVarCounter.containsKey(userDefinedName)){
+    		System.out.println("Var has already been defined");
+    	}
+    	userVarCounter.put(userDefinedName, 0);
 
     	ST output = group.getInstanceOf("declareIntVar");
     	ST output2 = output.add("varName", getCurrentForUserVar(scope, userDefinedName));
-    	visit(ctx.assignment.expr());
+    	programBody = programBody + "\n" + output.render();
+    	visit(ctx.assignment());
     	return null;
     }
 
     @Override
     public Void visitDeclNoAsn(VCalcParser.DeclNoAsnContext ctx) {
     	String userDefinedName = ctx.ID().getText();
-    	ST output = group.getInstanceOf("declareIntVar")
+
+    	if (userVarCounter.containsKey(userDefinedName)){
+    		System.out.println("Var has already been defined");
+    	}
+    	userVarCounter.put(userDefinedName, 0);
+
+    	ST output = group.getInstanceOf("declareIntVar");
     	ST output2 = output.add("varName", getCurrentForUserVar(scope, userDefinedName));
+    	programBody = programBody + "\n" + output.render();
     	return null;
     }
 
     @Override
     public Void visitAssignment(VCalcParser.AssignmentContext ctx) {
     	String userDefinedName = ctx.ID().getText();
+    	visit(ctx.expr());
+    	String exprResult = getCurrentVar();
     	ST output = group.getInstanceOf("assignIntToVar");
-    	ST output2 = output.add("varName", getNextForUserVar(scope, userDefinedName));
-    	ST output3 = output.add("assignResult", getCurrentVar());
-    	ST output3 = output.add("tempVar1", getNextVar());
+    	ST output2 = output.add("varName", getCurrentForUserVar(scope, userDefinedName));
+    	ST output3 = output.add("assignResult", exprResult);
+    	ST output4 = output.add("tempVar1", getNextVar());
+    	programBody = programBody + "\n" + output.render();
     	return null;
     }
 
@@ -81,6 +103,17 @@ public class LLVMVisitor extends VCalcBaseVisitor<Void> {
 	    programBody = programBody + "\n" + output.render();
 	    return null;
 	}
+
+	@Override
+    public Void visitExprId(VCalcParser.ExprIdContext ctx) {
+        String userDefinedName = ctx.ID().getText();
+
+        ST output = group.getInstanceOf("writeIntIdToVar");
+        ST output2 = output.add("varName", getCurrentForUserVar(scope, userDefinedName));
+        ST output3 = output.add("resultVar", getNextVar());
+        programBody = programBody + "\n" + output.render();
+        return null;
+    }
 
 	@Override
 	public Void visitExprInt(VCalcParser.ExprIntContext ctx) {
@@ -199,7 +232,7 @@ public class LLVMVisitor extends VCalcBaseVisitor<Void> {
 
 
     private String getCurrentVar() {
-    	return "t" + varCounter.toString();
+    	return "var" + varCounter.toString();
     }
 
     private String getNextVar() {
@@ -207,11 +240,14 @@ public class LLVMVisitor extends VCalcBaseVisitor<Void> {
     	return this.getCurrentVar();
     }
 
-    private String getNextForUserVar(scope, userDefinedName) {
-
+    private String getCurrentForUserVar(Scope scope, String userDefinedName) {
+    	Integer userVarCount = userVarCounter.get(userDefinedName);
+    	return "s" + scopeCounter.toString() + userDefinedName + userVarCount.toString();
     }
 
-    private String getCurrentForUserVar(scope, userDefinedName) {
-
+    private String getNextForUserVar(Scope scope, String userDefinedName) {
+    	Integer userVarCount = userVarCounter.get(userDefinedName) + 1;
+    	userVarCounter.put(userDefinedName, userVarCount);
+    	return this.getCurrentForUserVar(scope, userDefinedName);
     }
 }
